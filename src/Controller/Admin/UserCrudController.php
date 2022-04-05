2a867bfcef6f282b2f3fb1850709b86953120571
theoxav <theoxav971@outlook.fr>
2022-04-05 17:37:11 +0200

    install easyadminbundle, configure DashboardController create UserCrudController and configure

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserCrudController extends AbstractCrudController
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher, private EntityRepository $entityRepo ){}
+   
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    // Permet de ne pas afficher l'utilisateur courant 
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $userId = $this->getUser()->getId();
+
+        $response = $this->entityRepo->createQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        $response->andWhere('entity.id != :userId')->setParameter('userId', $userId);
+
+        return $response;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+    
+           yield TextField::new('firstName');
+           yield TextField::new('lastName');
+
+           yield EmailField::new('email');
+
+           yield TextField::new('password')->onlyOnForms()
+             ->setFormType(PasswordType::class);
+            
+           yield ChoiceField::new('roles')
+             ->allowMultipleChoices()
+             ->renderAsBadges([
+                 'ROLE_ADMIN' => 'success',
+                 'MANAGER' => 'warning'
+             ])
+             ->setChoices([
+                 'Administrator' => 'ROLE_ADMIN',
+                 'Manager' => 'ROLE_MANAGER'
+             ]) 
+        ;
+    }
+
+    public function persistEntity(EntityManagerInterface $em, $entityInstance): void
+    {
+        $user = $entityInstance;
+
+        $plainPassword = $user->getPassword();
+        $hashPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+
+        $user->setPassword($hashPassword);
+
+        parent::persistEntity($em, $user);
+    }
+    
+}
